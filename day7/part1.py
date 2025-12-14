eebd9d4928f1a1069d7f7676ec0f16cfceed4355
André Alves <nseiporamor@gmail.com>
2025-12-14 23:42:59 +0000

    Advent of code solutions for both part1 and 2 of day 7 by me

---
+++ day7/part1.py
@@ -0,0 +1,23 @@
+from icecream import ic
+
+smallInput = "inputSmall.txt"
+bigInput = "inputBig.txt"
+
+total = 0
+with open(bigInput, "r") as file:
+    lines = file.readlines()
+    beamsLocations = ''.zfill(len(lines[0].strip()))
+    beamsLocations = list(beamsLocations)
+    for index, char in enumerate(lines[0].strip()):
+        if char == 'S':
+            beamsLocations[index] = '1'
+            break
+    for index1, line in enumerate(lines[1:]):
+        line = line.strip()
+        for index, char in enumerate(line):
+            if char == '^' and beamsLocations[index] == '1':
+                beamsLocations[index] = '0'
+                beamsLocations[index-1] = '1'
+                beamsLocations[index+1] = '1'
+                total += 1
+    ic(total)
